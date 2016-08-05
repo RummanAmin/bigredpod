7285a2a40aa48eeb11a97d067869b442624dc986
Rumman Amin <Rumman.amin10@gmail.com>
2016-08-05 13:26:48 +0100

    Added GA Code

--- footer.php
+++ footer.php
@@ -48,7 +48,7 @@
       e=o.createElement(i);r=o.getElementsByTagName(i)[0];
       e.src='https://www.google-analytics.com/analytics.js';
       r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-      ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+      ga('create','UA-82049525-1','auto');ga('send','pageview');
     </script>
 
     <?php wp_footer(); ?>
