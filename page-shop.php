55c7c5f8c630a66f7d691af6d50a0785652d36be
Rumman Amin <Rumman.amin10@gmail.com>
2016-10-18 22:25:25 +0100

    Shop
    Images and Template for Shop

---
+++ page-shop.php
@@ -0,0 +1,282 @@
+<?php get_header(); ?>
+
+  <div class="band header">
+    <section class="layout">
+    	<ul>
+    		<li>Categories:</li>
+    		<li><a href="#one">Media</a></li>
+    		<li><a href="#two">Gifts</a></li>
+    		<li><a href="#three">Clothes</a></li>
+    		<li><a href="#four">Accessories</a></li>
+        <li><a href="#five">Home</a></li>
+    	</ul>
+    </section>
+  </div>
+
+  <div class="band main">
+  	<section class="layout">
+      <div class="shop-shelf">
+        <a name="one"></a>
+        <h4>Media</h4>
+        <ul class="list-grid">
+          <li>
+            <a href="http://amzn.to/2cWR2z2" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/07-review.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cWR2z2" target="_blank">2007/08 Season Review</a></p>
+              <p class="item-price">£3.53</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2cD4k7r" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/utd-legends.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cD4k7r" target="_blank">Manchester United Legends</a></p>
+              <p class="item-price">£8.00</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d5KGza" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/busby-era.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d5KGza" target="_blank">The Busby Era</a></p>
+              <p class="item-price">£9.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duCPxU" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/2013-review.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duCPxU" target="_blank">2012/13 Season Review</a></p>
+              <p class="item-price">£19.69</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d5KQ9W" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/98-review.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d5KQ9W" target="_blank">1998/99 Season Review</a></p>
+              <p class="item-price">£12.65</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d80eps" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/class-92.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d80eps" target="_blank">The Class of '92</a></p>
+              <p class="item-price">£8.99</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+  		<div class="shop-shelf">
+        <a name="two"></a>
+  			<h4>Gifts</h4>
+  			<ul class="list-grid">
+          <li>
+            <a href="http://amzn.to/2duzaQG" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/keyring.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duzaQG" target="_blank">Crest Keyring</a></p>
+              <p class="item-price">£3.50</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2cWOII0" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/mug.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cWOII0" target="_blank">Football Mug</a></p>
+              <p class="item-price">£6.49</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2e5tWvR" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/bowl.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2e5tWvR" target="_blank">Breakfast Bowl</a></p>
+              <p class="item-price">£10.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duzQW2" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/calendar.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duzQW2" target="_blank">2017 Calendar</a></p>
+              <p class="item-price">£8.27</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2dsENum" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/saf-book.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2dsENum" target="_blank">Sir Alex Ferguson Autobiography</a></p>
+              <p class="item-price">£7.49</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duAzqm" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/zlatan-book.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duAzqm" target="_blank">I Am Zlatan Ibrahimovic</a></p>
+              <p class="item-price">£6.29</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d5KdgE" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/giftwrap.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d5KdgE" target="_blank">Gift Wrap & Tags</a></p>
+              <p class="item-price">£1.20</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d5LS5G" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/apron.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d5LS5G" target="_blank">Home Kit Apron</a></p>
+              <p class="item-price">£14.99</p>
+            </div>
+          </li>
+        </ul>
+  		</div>
+  		<div class="shop-shelf">
+        <a name="three"></a>
+  			<h4>Clothes</h4>
+  			<ul class="list-grid">
+          <li>
+            <a href="http://amzn.to/2duD8bV" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/home-kit.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duD8bV" target="_blank">2016/17 Official Home Kit</a></p>
+              <p class="item-price">£48.45</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duAXoX" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/pyjamas.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duAXoX" target="_blank">Pyjama Bottoms</a></p>
+              <p class="item-price">£12.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2dsCqIg" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/polo.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2dsCqIg" target="_blank">Crest Polo Shirt</a></p>
+              <p class="item-price">£17.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2dsCGH5" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/robe.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2dsCGH5" target="_blank">Hooded Bath Robe</a></p>
+              <p class="item-price">£27.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2cD6HqM" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/tshirt.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cD6HqM" target="_blank">Official Crest T-Shirt</a></p>
+              <p class="item-price">£9.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duBC9P" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/track-top.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duBC9P" target="_blank">Official Track Jacket</a></p>
+              <p class="item-price">£37.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2efjT41" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/hoody.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2efjT41" target="_blank">Official Fleece Hoody</a></p>
+              <p class="item-price">£37.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2eikz7g" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/windbreaker.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2eikz7g" target="_blank">Windbreaker Jacket</a></p>
+              <p class="item-price">£37.99</p>
+            </div>
+          </li>
+        </ul>
+  		</div>
+      <div class="shop-shelf">
+        <a name="four"></a>
+        <h4>Accessories</h4>
+        <ul class="list-grid">
+          <li>
+            <a href="http://amzn.to/2d7X6df" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/socks.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d7X6df" target="_blank">2 Pairs of Socks</a></p>
+              <p class="item-price">£5.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d7WMeB" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/beanie.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d7WMeB" target="_blank">Red Beanie Hat</a></p>
+              <p class="item-price">£5.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2dsBm72" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/beanie-black.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2dsBm72" target="_blank">Black Beanie Hat</a></p>
+              <p class="item-price">£9.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d7YuN2" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/wallet.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d7YuN2" target="_blank">Embossed Leather Wallet</a></p>
+              <p class="item-price">£15.90</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duAMtw" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/tie.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duAMtw" target="_blank">Official Club Tie</a></p>
+              <p class="item-price">£9.99</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+  		<div class="shop-shelf">
+        <a name="five"></a>
+  			<h4>Home</h4>
+  			<ul class="list-grid">
+          <li>
+            <a href="http://amzn.to/2d7YIDL" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/sign.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d7YIDL" target="_blank">Official Street Sign</a></p>
+              <p class="item-price">£4.30</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2cD2gfZ" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/stickers.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cD2gfZ" target="_blank">Wall Stickers</a></p>
+              <p class="item-price">£5.99</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d5GO1c" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/rug.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d5GO1c" target="_blank">Crest Rug</a></p>
+              <p class="item-price">£11.29</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2duAoeT" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/curtains.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2duAoeT" target="_blank">Curtains</a></p>
+              <p class="item-price">£21.49</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2d7Xqc5" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/duvet.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2d7Xqc5" target="_blank">Duvet Set</a></p>
+              <p class="item-price">£18.35</p>
+            </div>
+          </li>
+          <li>
+            <a href="http://amzn.to/2cD4hIP" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ) ?>/img/products/cushion.jpg"></a>
+            <div class="item-info">
+              <p class="item-title"><a href="http://amzn.to/2cD4hIP" target="_blank">Cushion</a></p>
+              <p class="item-price">£12.38</p>
+            </div>
+          </li>
+        </ul>
+  		</div>
+  	</section>
+  </div>
+
+<?php get_footer(); ?>
